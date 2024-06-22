0b81d5ace9801a7f9eff290e902d0346bfd93c9f
Angellomon <angelmtzdiaz@gmail.com>
2024-06-21 18:26:34 -0600

    git: init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.css';
+</script>
+
+<svelte:head>
+	<title>Constancas Seminario de Aduanas | Ciclo 2024</title>
+</svelte:head>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Constancas Seminario de Aduanas | Ciclo 2024</h1>
