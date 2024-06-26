7aabecc79a0152555b30e753b55f30d1d55c4929
Angellomon <angelmtzdiaz@gmail.com>
2024-06-26 02:17:20 -0600

    feat(server): impl proxy

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		proxy: {
+			'/pdf/12otozn0bz': 'https://constancias.umbrellaservices.angellos.net'
+		}
+	},
+	ssr: {
+		external: ['file-saver']
+	}
 });
