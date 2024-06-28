98c054ff8f7c27be8c157c873320034901770bfd
Angellomon <angelmtzdiaz@gmail.com>
2024-06-28 17:41:30 -0600

    feat(lib): impl header title component

---
+++ src/lib/header-title.svelte
@@ -0,0 +1,18 @@
+<script>
+	export let title = 'CONSTANCIAS';
+</script>
+
+<header class="flex flex-col-reverse sm:flex-row relative items-center justify-evenly py-10">
+	<div
+		class="absolute z-0 bg-cargo-img-md xl:bg-cargo-img-lg 2xl:bg-cargo-img-xl bg-no-repeat bg-right-bottom sm:bg-[length:70%] bg-cover h-full w-full"
+	></div>
+	<div
+		class="absolute hidden sm:block z-10 bg-gradient-to-r from-white from-40% to-transparent h-full w-full"
+	></div>
+	<div class="sm:hidden absolute border-4 border-white h-[95%] w-[95%]"></div>
+
+	<h1 class="mb-5 z-20 py-2 px-12 sm:text-4xl text-2xl text-white bg-ecstasy inline-flex">
+		<strong> {title} </strong>
+	</h1>
+	<img class="mb-5 z-20 max-h-[25vh]" src="/img/logo-seminario.png" alt="logo seminario 2024" />
+</header>
