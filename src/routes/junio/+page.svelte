3d31dd898b8aff7de2def15d028bf97571bcd02e
Angellomon <angelmtzdiaz@gmail.com>
2024-06-28 17:42:27 -0600

    feat(events): impl june route

--- src/routes/junio/+page.svelte
+++ src/routes/junio/+page.svelte
@@ -1,5 +1,7 @@
 <script>
 	import { browser } from '$app/environment';
+	import Footer from '$lib/footer.svelte';
+	import HeaderTitle from '$lib/header-title.svelte';
 	import { downloadCertificate } from '$lib/requests';
 
 	let email = '';
@@ -8,7 +10,11 @@
 	let notFound = false;
 
 	async function download() {
-		if (!email || !browser) return;
+		if (!email || !browser) {
+			invalidEmail = invalidEmail = true;
+
+			return;
+		}
 
 		loading = true;
 		invalidEmail = false;
@@ -23,40 +29,44 @@
 	}
 </script>
 
-<h1>Descarga de Constancia</h1>
+<section class="relative bg-planes-pattern-md xl:bg-planes-pattern-lg 2xl:bg-planes-pattern-xl">
+	<HeaderTitle title="JUNIO" />
 
-<div class="flex flex-row">
-	<input
-		bind:value={email}
-		class="p-2 border border-slate"
-		type="email"
-		name="email"
-		id="email"
-		placeholder="correo"
-	/>
-	<button
-		disabled={loading}
-		class="p-2 bg-slate-100 border-t border-r border-b border-slate hover:underline"
-		on:click={download}
-	>
-		{#if loading}
-			Cargando
-		{:else}
-			Descargar
+	<main class=" flex flex-col justify-evenly items-center min-h-[30vh] sm:min-h-[50vh]">
+		<p class="text-center">Ingresa el correo electrónico con el cual te registraste</p>
+
+		<div class="flex flex-row">
+			<input
+				bind:value={email}
+				class="p-2 px-6 border border-ecstasy-200 text-ecstasy-800 rounded-l-full w-[60vw] sm:w-[30vw]"
+				type="email"
+				name="email"
+				id="email"
+				placeholder="correo"
+			/>
+			<button
+				disabled={loading}
+				class="p-2 px-6 bg-ecstasy text-white border-t border-r border-b border-ecstasy hover:underline rounded-r-full"
+				on:click={download}
+			>
+				<strong
+					>{#if loading}
+						Cargando
+					{:else}
+						Enviar
+					{/if}</strong
+				>
+			</button>
+		</div>
+
+		{#if invalidEmail}
+			<span class="text-red-600">Correo inválido</span>
 		{/if}
-	</button>
-</div>
 
-{#if invalidEmail}
-	<span class="text-red-600">Correo inválido</span>
-{/if}
+		{#if notFound}
+			<span class="text-red-600">Correo no encontrado</span>
+		{/if}
+	</main>
 
-{#if notFound}
-	<span class="text-red-600">Correo no encontrado</span>
-{/if}
-
-<!-- svelte-ignore a11y-click-events-have-key-events -->
-<!-- svelte-ignore a11y-no-static-element-interactions -->
-<pre>test email: <span class="cursor-pointer" on:click={() => (email = 'test@test.com')}
-		>test@test.com</span
-	></pre>
+	<Footer />
+</section>
