bf7c80e118f69e8d19354bd6fd592a9ccf9b3bf4
Angellomon <angelmtzdiaz@gmail.com>
2024-06-28 16:01:11 -0600

    feat(pages): impl main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,26 @@
-<h1>Constancias Seminario de Aduanas | Ciclo 2024</h1>
-<b>Constancias Seminario de Aduanas | Ciclo 2024</b>
+<header class="flex relative items-center justify-evenly min-h-[30vh]">
+	<div
+		class="absolute z-0 bg-cargo-img-md xl:bg-cargo-img-lg 2xl:bg-cargo-img-xl bg-no-repeat bg-right-bottom bg-[length:60%] h-full w-full"
+	></div>
+	<div
+		class="absolute z-10 bg-gradient-to-r from-white from-40% to-transparent h-full w-full"
+	></div>
+	<h1 class="z-20 py-2 px-12 text-4xl text-white bg-ecstasy inline-flex">
+		<strong> CONSTANCIAS </strong>
+	</h1>
+	<img class="z-20 max-h-[25vh]" src="/img/logo-seminario.png" alt="logo seminario 2024" />
+</header>
 
-<p>Landing</p>
+<main class="bg-planes-pattern-md xl:bg-planes-pattern-lg 2xl:bg-planes-pattern-xl">
+	<p class="text-2xl text-center my-16 text-deep-oak">INDICA A QUÉ EDICIÓN ASISTISTE</p>
 
-<a href="/constancia">
-	<button class="m-2 px-3 py-2 border border-slate">Descargar Constancia</button>
-</a>
-
-<style>
-</style>
+	<div class="flex justify-center items-center">
+		<a href="/junio">
+			<button
+				class="py-4 px-8 shadow shadow-slate-800 rounded-full bg-slate-100 border-4 border-slate-300"
+			>
+				<strong class="text-ecstasy text-2xl"> JUNIO </strong>
+			</button>
+		</a>
+	</div>
+</main>
