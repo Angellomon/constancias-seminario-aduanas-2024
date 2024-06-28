129017bcf2986fe66d1df83b40fd9c1c6ab4aa1b
Angellomon <angelmtzdiaz@gmail.com>
2024-06-28 17:11:31 -0600

    feat(pages): impl main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,17 +3,19 @@
 </script>
 
 <section class="relative bg-planes-pattern-md xl:bg-planes-pattern-lg 2xl:bg-planes-pattern-xl">
-	<header class="flex flex-wrap relative items-center justify-evenly py-10">
+	<header class="flex flex-col-reverse sm:flex-row relative items-center justify-evenly py-10">
 		<div
 			class="absolute z-0 bg-cargo-img-md xl:bg-cargo-img-lg 2xl:bg-cargo-img-xl bg-no-repeat bg-right-bottom sm:bg-[length:70%] bg-cover h-full w-full"
 		></div>
 		<div
-			class="absolute z-10 bg-gradient-to-r from-white from-40% to-transparent h-full w-full"
+			class="absolute hidden sm:block z-10 bg-gradient-to-r from-white from-40% to-transparent h-full w-full"
 		></div>
+		<div class="absolute border-4 border-white h-[95%] w-[95%]"></div>
+
 		<h1 class="mb-5 z-20 py-2 px-12 sm:text-4xl text-2xl text-white bg-ecstasy inline-flex">
 			<strong> CONSTANCIAS </strong>
 		</h1>
-		<img class="z-20 max-h-[25vh]" src="/img/logo-seminario.png" alt="logo seminario 2024" />
+		<img class="mb-5 z-20 max-h-[25vh]" src="/img/logo-seminario.png" alt="logo seminario 2024" />
 	</header>
 
 	<main class="my-10 min-h-[30vh]">
