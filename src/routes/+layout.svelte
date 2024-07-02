d2e86ccd6c26d3ead04e82be17dc5f51b15af841
Angellomon <angelmtzdiaz@gmail.com>
2024-07-02 11:40:15 -0600

    feat: impl open graph protocol

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,14 @@
 
 <svelte:head>
 	<title>Constancias Seminario de Aduanas | Ciclo 2024</title>
+	<meta property="og:url" content="https://constancias-seminario-aduanas-2024.onrender.com/" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Constancias Seminario de Aduanas | Ciclo 2024" />
+	<meta property="og:description" content="" />
+	<meta
+		property="og:image"
+		content="https://constancias-seminario-aduanas-2024.onrender.com/favicon.png"
+	/>
 </svelte:head>
 
 <slot />
